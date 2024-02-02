7516a8e6a7ccca2c8610705d294ade725ce96af6
sath14 <73119373+sath14@users.noreply.github.com>
2024-02-03 00:07:25 +0800

    Add files via upload

---
+++ contactform.php
@@ -0,0 +1,19 @@
+<?php
+
+if(isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $subject = $_POST['subject'];
+  $mailform = $_POST['mail'];
+  $message = $_POST['message'];
+
+  $mailTo = "sath50@hotmail.com";
+  $headers = "From: ".$mailFrom
+  $txt = "You have received an e-mail from".$name.".\n\n".$message;
+
+ 
+
+
+
+  mail($mailTo,$txt,$headers);
+  header("Location: index.php?mailsend");
+}
